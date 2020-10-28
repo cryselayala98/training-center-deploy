b7c08bdfce09ec62f7f9d652fa9bee20516939a7
criselayala98 <criseljazminal@ufps.edu.co>
2020-10-27 21:27:15 -0500

    cambios impotantes al dockerfile del front y al docker compose para ejecutar el front sobre nginx

--- update-repo.sh
+++ update-repo.sh
@@ -1,15 +1,26 @@
-
 #!/bin/bash
 
 echo Updating Training Center......
 echo Pulling from Training-Center master branch....
-
-sleep 1s
+sleep 3s
 
 git pull origin master
 
-echo Building and starting docker containers in the background...
+echo Building front with Aurelia......
+sleep 3s
 
-sleep 1s
+cd frontend
+au build
+cd ..
 
-docker-compose up --build -d
+echo Listo, el container del front ha sido sincronzado con los cambios del proyecto.
+
+#echo actualizando los scripts del container frontend...
+#docker exec -d training-center-frontend 
+
+
+#echo Building and starting docker containers in the background...
+
+#sleep 1s
+
+#docker-compose up --build -d
