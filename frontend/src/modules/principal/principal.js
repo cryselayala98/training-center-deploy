2eccc2bcaf20b06ce790925c3ca02967ea97583e
andeley <andeley23@gmail.com>
2020-11-02 22:55:56 -0500

    arreglando principal

--- frontend/src/modules/principal/principal.js
+++ frontend/src/modules/principal/principal.js
@@ -1,3 +1,18 @@
+import { inject, observable } from "aurelia-framework";
+
+import { MESSAGES } from "config/config";
+
+@inject(Alert, Auth)
 export class Principal {
-  
+  @observable page
+  @observable filterChange
+
+  /**
+   * Inicializa una instancia de Admin.
+   * @param {service} alertService - Servicio de notificaciones
+   * @param {service} authService - Servicio de autenticación y validación
+  */
+ constructor (alertService, authService, problemService, rankingService) {
+  this.alertService = alertService
+  this.authService = authService
 }
