6a62f7025e1ef135711f94a769d863a863b5d414
criselayala98 <criseljazminal@ufps.edu.co>
2020-11-03 12:35:08 -0500

    Enmaquetación del módulo de editor online y del servicio de Blockly custom

--- frontend/src/modules/problems/problem.js
+++ frontend/src/modules/problems/problem.js
@@ -1,57 +1,65 @@
 export class Problem {
     /**
-   * Se encarga del enrutamiento dentro de la aplicación
-   * @param {any} config - Configuración de la aplicación
-   * @param {any} router - Enrutador principal de la aplicación
-   */
-  configureRouter (config, router) {
-    config.map([
-      {
-        route: '',
-        name: 'problems',
-        moduleId: 'modules/problems/general-problems/general-problems',
-        title: 'Problemas',
-        settings: {
-          roles: ['admin', 'coach', 'student']
-        }
-      },
-      {
-        name: 'category',
-        route: 'categoria/:id',
-        moduleId: 'modules/problems/category-problems/category-problems',
-        title: 'Problemas',
-        settings: {
-          roles: ['admin', 'coach', 'student']
-        }
-      },
-      {
-        name: 'problems-creator',
-        route: 'nuevo',
-        moduleId: 'modules/problems/problems-creator/problems-creator',
-        title: 'Nuevo problema',
-        settings: {
-          roles: ['admin', 'coach']
-        }
-      },
-      {
-        name: 'edit-problem',
-        route: ':id/editar',
-        moduleId: 'modules/problems/problems-creator/problems-editor',
-        title: 'Editar problema',
-        settings: {
-          roles: ['admin', 'coach']
-        }
-      },
-      {
-        name: 'view-problem',
-        route: [':id/detalle', ':id/detalle/:lang'],
-        moduleId: 'modules/problems/view-problem/view-problem',
-        title: 'Problema',
-        settings: {
-          roles: ['admin', 'coach', 'student']
-        }
-      }
-    ])
-    this.router = router
-  }
+     * Se encarga del enrutamiento dentro de la aplicación
+     * @param {any} config - Configuración de la aplicación
+     * @param {any} router - Enrutador principal de la aplicación
+     */
+    configureRouter(config, router) {
+        config.map([{
+                route: '',
+                name: 'problems',
+                moduleId: 'modules/problems/general-problems/general-problems',
+                title: 'Problemas',
+                settings: {
+                    roles: ['admin', 'coach', 'student']
+                }
+            },
+            {
+                name: 'category',
+                route: 'categoria/:id',
+                moduleId: 'modules/problems/category-problems/category-problems',
+                title: 'Problemas',
+                settings: {
+                    roles: ['admin', 'coach', 'student']
+                }
+            },
+            {
+                name: 'problems-creator',
+                route: 'nuevo',
+                moduleId: 'modules/problems/problems-creator/problems-creator',
+                title: 'Nuevo problema',
+                settings: {
+                    roles: ['admin', 'coach']
+                }
+            },
+            {
+                name: 'edit-problem',
+                route: ':id/editar',
+                moduleId: 'modules/problems/problems-creator/problems-editor',
+                title: 'Editar problema',
+                settings: {
+                    roles: ['admin', 'coach']
+                }
+            },
+            {
+                name: 'view-problem',
+                route: [':id/detalle', ':id/detalle/:lang'],
+                moduleId: 'modules/problems/view-problem/view-problem',
+                title: 'Problema',
+                settings: {
+                    roles: ['admin', 'coach', 'student']
+                }
+            },
+            {
+                name: 'editor-online',
+                route: 'bloques', //route: ':id/bloques',
+                moduleId: 'modules/problems/problem-editor-online/problem-editor-online',
+                title: 'Envía tu solución con bloques',
+                settings: {
+                    roles: ['visitor']
+                }
+            }
+        ])
+        this.router = router
+    }
 }
---
+++ frontend/src/services/blockly-custom.js
@@ -0,0 +1,19 @@
+import Blockly from 'blockly'
+
+/**
+ * Servicio para la creación de bloques personalizados
+ * 
+ * */
+
+export class BlocklyCustom {
+
+    constructor() {
+        /* var workspace = Blockly.inject('blocklyDiv', { toolbox: document.getElementById('toolbox') });*/
+
+
+        //console.log(Blockly);
+
+    }
+
+
+}
