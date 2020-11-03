6d95fb5f0336601394cbd24cdf9fde082db82233
andeley <andeley23@gmail.com>
2020-11-02 23:31:37 -0500

    arreglando principal

--- frontend/src/modules/principal/principal.js
+++ frontend/src/modules/principal/principal.js
@@ -1,5 +1,5 @@
 import { inject, observable } from "aurelia-framework";
-
+import { Router } from 'aurelia-router'
 import { MESSAGES } from "config/config";
 
 @inject(Alert, Auth)
@@ -11,8 +11,22 @@ export class Principal {
    * Inicializa una instancia de Admin.
    * @param {service} alertService - Servicio de notificaciones
    * @param {service} authService - Servicio de autenticación y validación
-  */
+   */
  constructor (alertService, authService, problemService, rankingService) {
   this.alertService = alertService
   this.authService = authService
+  this.routerService = routerService
+}
+
+/**
+ * Método que toma los parametros enviados en el link y configura la página para adaptarse
+ * al contenido solicitado. Este método hace parte del ciclo de vida de la aplicación y se
+ * ejecuta automáticamente luego de lanzar el constructor.
+ * @param {any} params
+ * @param {any} routeConfig
+ */
+activate (params, routeConfig) {
+  this.routeConfig = routeConfig
+  this.id = params.id
+}
 }
